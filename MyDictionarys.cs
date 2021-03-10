3a8f781602caf0b5072f2338ce2ba74abffff872
tahabasturk <basturktaha3@gmail.com>
2021-03-10 22:06:17 +0200

    ilk gönderme

---
+++ MyDictionarys.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionarys<T,X>
+    {
+        T[] keys;
+        X[] values;
+        T[] geciciKeys;
+        X[] geciciValues;
+        public MyDictionarys()
+        {
+            keys = new T[0];
+            values = new X[0];
+        }
+        public void Add(T key,X value)
+        {
+            geciciKeys = keys;
+            keys = new T[keys.Length + 1];
+            geciciValues = values;
+            values = new X[values.Length + 1];
+            for (int i = 0; i < geciciKeys.Length; i++)
+            {
+                keys[i] = geciciKeys[i];
+                values[i] = geciciValues[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+        public T[] Keys
+        {
+            get { return keys; }
+        }
+        public X[] Values
+        {
+            get { return values; }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            
+
+            MyDictionarys<int, string> isimler = new MyDictionarys<int, string>();
+            isimler.Add(1, "Taha");
+            isimler.Add(2, "Ali");
+            isimler.Add(3, "Hasan");
+            isimler.Add(4, "Yücel");
+            Console.WriteLine(isimler.Count);
+            foreach (var isim in isimler.Values)
+            {
+                Console.WriteLine(isim);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
